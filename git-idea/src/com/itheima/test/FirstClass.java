0d3a3d26e0332450708893b87a64e8b3a583d4ac
time <test@time.com>
2019-07-20 11:34:54 +0800

    把本地工程添加到Git

---
+++ git-idea/src/com/itheima/test/FirstClass.java
@@ -0,0 +1,12 @@
+package com.itheima.test;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Git Project'");
+        System.out.println("Hello Git Project'");
+        System.out.println("Hello Git Project'");
+        System.out.println("Hello Git Project'");
+    }
+}
